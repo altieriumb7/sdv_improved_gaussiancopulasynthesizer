d99b2b6658fd5a5b943755f794cbcf5e9f3aa47a
Umberto Altieri <103131160+altieriumb7@users.noreply.github.com>
2025-08-28 02:04:19 +0200

    Update __init__.py

--- src/new_sdv/__init__.py
+++ src/new_sdv/__init__.py
@@ -1,4 +1,1 @@
-__version__ = "0.1.0"
-
-from .synthesizer import GaussianCopulaSynthesizer
-from .constraints import Constraint, UniqueConstraint, RangeConstraint
+from .synthesizer import GaussianCopulaSynthesizer, RangeConstraint, UniqueConstraint
